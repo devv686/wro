627c09b46262ee111eb2e5890539d332078be8d0
devv686 <158228923+devv686@users.noreply.github.com>
2024-01-30 19:26:20 -0500

    Create miscFunctions.c

---
+++ miscFunctions.c
@@ -0,0 +1,63 @@
+float getTrueDegrees(int theMotorEncoder){
+  if (theMotorEncoder){
+    return getMotorEncoder(right);
+  }
+  else{
+    return getMotorEncoder(left)*-0.99;   
+  }
+}
+
+float getAbsoluteDegrees(int theMotorEncoder){ 
+  if (theMotorEncoder == true){
+    return getTrueDegrees(theMotorEncoder) - absoluteRight;
+  }
+  else{
+    return getTrueDegrees(theMotorEncoder) - absoluteLeft;
+  }
+} // wall/line squaring stuff
+
+float getRelativeDegrees(){
+  if (theMotorEncoder){
+    return getAbsoluteDegrees(theMotorEncoder) - relativeBaseRight;
+  }
+  else{
+    return getAbsoluteDegrees(theMotorEcoder) - relativeBaseLeft;
+  }
+}
+
+void resetRelative(){
+  relatievBaseLeft = getAbsoluteDegrees(LEFT);
+  relativeBaseRight = getAbsoluteDegrees(RIGHT);
+}
+
+void resetMotors(){
+  relativeBaseLeft = 0;
+  relativeBaseRight = 0;
+  absoluteLeft = getAbsoluteDegrees(LEFT);
+  absoluteRight = getAbsoluteDegrees(RIGHT);
+}
+
+void setSpeeds(float rSpeed, float lSpeed){
+  if (rSpeed>100){
+    lSpeed *= 100/rSpeed;
+    rSpeed = 100;
+  }else if (rSpeed<-100){
+    lSpeed *= -100/rSpeed;
+    rSpeed = -100;
+  }
+
+  if (lSpeed>100){
+    rSpeed *= 100/lSpeed;
+    lSpeed = 100;
+  } else if (lSpeed<-100){
+    rSpeed *= -100/lSpeed;
+    lSpeed = -100;
+  }
+
+  // !!finish and include the speed adjustment based on the ratio 
+}
+
+float degToCM(){
+
+
+
